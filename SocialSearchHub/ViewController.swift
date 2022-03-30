ab7e785dee4e02695ff02f988c8bd40cad719d20
Payton Dennis <dennispayton15@gmail.com>
2022-03-29 20:12:56 -0400

    Initial Commit

---
+++ SocialSearchHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialSearchHub
+//
+//  Created by Eva Keeling on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
