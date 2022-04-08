dd720cacdfb1fcca8d40a87920b8d825bfa96231
Payton Dennis <dennispayton15@gmail.com>
2022-04-08 08:21:19 -0400

    Profile page displays favorite searches

--- SocialSearchHub/ProfileViewController.swift
+++ SocialSearchHub/ProfileViewController.swift
@@ -7,16 +7,19 @@
 // OWNER: Payton
 
 import UIKit
+import Parse
 
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var favoriteSearches = NSArray()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return favoriteSearches.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteSearchTermCell", for: indexPath) as! FavoriteSearchTermTableViewCell
         
-        cell.favoriteSearchTerm.text = "Hello World!"
+        cell.favoriteSearchTerm.text = favoriteSearches[indexPath.row] as! String
         
         return cell
     }
@@ -35,6 +38,24 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getUserInfoFromDB()
+    }
+    
+    func getUserInfoFromDB() {
+        // Special query for User object
+        // https://docs.parseplatform.org/ios/guide/#querying
+        var favoriteSearches = NSArray()
+        let objectId = PFUser.current()?.objectId
+        let query = PFUser.query()
+        do {
+            let user = try PFUser.query()?.getObjectWithId(objectId!)
+            favoriteSearches = user!["favoriteSearches"] as! NSArray
+        } catch {
+            print("Error occured")
+        }
+        
+        self.favoriteSearches = favoriteSearches
     }
     
 
