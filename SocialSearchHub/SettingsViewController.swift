27e2939ec5964222980e196df040d5c064449da8
Payton Dennis <dennispayton15@gmail.com>
2022-04-13 07:33:25 -0400

    Dark Mode is updated on toggle

--- SocialSearchHub/SettingsViewController.swift
+++ SocialSearchHub/SettingsViewController.swift
@@ -25,6 +25,55 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.darkModeSwitch.isOn = getDarkModeValue()
+    }
+    
+    @IBAction func onDarkModeSwitchValueChange(_ sender: Any) {
+        // Toggle dark mode
+        
+        // Get App Delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+           return
+        }
+        
+        
+        if (darkModeSwitch.isOn) {
+            // Update screens
+            
+        } else {
+            // Update screens
+            
+        }
+        
+        // Save user choice database
+        do {
+            let user = PFUser.current()
+            user?["darkMode"] = self.darkModeSwitch.isOn
+            try user?.save()
+            print("Dark mode was updated")
+            
+        } catch {
+            print("Unable to update Dark Mode")
+
+        }
+    }
+    
+    func getDarkModeValue() -> Bool {
+        let objectId = PFUser.current()?.objectId
+        do {
+            let user = try PFUser.query()?.getObjectWithId(objectId!)
+            let darkMode = user!["darkMode"] as! Bool
+            
+            print(darkMode)
+            return darkMode
+            
+        } catch {
+            print("Unable to get Dark Mode from database")
+            
+            return false
+        }
+        
     }
     
 
